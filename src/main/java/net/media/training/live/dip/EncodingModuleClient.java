fa2cd083fd2749a3fb149d4fa5e85de1d9281361
pruthvi.n <pruthvirajgn@MMMDVPRUTHVIN.local>
2024-07-25 18:26:02 +0530

    Resolve DIP

--- src/main/java/net/media/training/live/dip/EncodingModuleClient.java
+++ src/main/java/net/media/training/live/dip/EncodingModuleClient.java
@@ -11,8 +11,9 @@ import java.io.IOException;
  */
 public class EncodingModuleClient {
     public static void main(String[] args) throws IOException {
-        EncodingModule encodingModule  = new EncodingModule();
-        encodingModule.encodeWithFiles();
-        encodingModule.encodeBasedOnNetworkAndDatabase();
+        EncodingModule encodingModulewithFiles  = new EncodeWithFiles();
+        EncodingModule encodingModuleBasedOnNetwork  = new EncodeBasedOnNetworkAndDatabase();
+        encodingModulewithFiles.encode();
+        encodingModuleBasedOnNetwork.encode();
     }
 }
--- src/main/java/net/media/training/live/dip/MyDatabase.java
+++ src/main/java/net/media/training/live/dip/MyDatabase.java
@@ -10,6 +10,7 @@ import java.util.Map;
  * Time: 4:47:27 PM
  * To change this template use File | Settings | File Templates.
  */
+
 public class MyDatabase {
     private static Map<Integer, String> data = new Hashtable<Integer, String>();
     private static int count = 0;
